00e71ae8b820afb2cdd4006cca3397d67fb7ad58
jonathanhumphrey <jlh473@txstate.edu>
2021-06-15 10:02:27 -0500

    styled popup modal

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../Style Sheets/FooterStyle.css'
 
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 import { useState } from 'react'
-
 
 
 export default function FooterComponent() {
     const [isVisible, setVisible] = useState(false)
-
+    const [id, setId] = useState(3)
+    
 
     return (
         <div className='footer'>
@@ -17,40 +18,71 @@ export default function FooterComponent() {
             <div className='footerRight'>
                 <ul className='footerList'>
                     <li>
-                        <a
-                            href='https://twitter.com/home' 
-                            target='_blank' 
-                            rel="noreferrer"
+                        <p
                             className='footerLinks'
-                        >Home</a>
+                            onClick={(event) => { setVisible(!isVisible);  setId(event.target.id)}}
+                            id='0'
+                        >Contact</p>
                     </li>
                     <li>
                         <p
                             className='footerLinks'
-                            onClick={() => setVisible(!isVisible)}
-                        >Contact</p>
+                            onClick={(event) => { setVisible(!isVisible);  setId(event.target.id)}}
+                            id='1'
+                        >FAQ</p>
                     </li>
                     <li>
-                        <a
-                            href='https://twitter.com/home' 
-                            target='_blank' 
-                            rel="noreferrer"
+                        <p
                             className='footerLinks'
-                        >F.A.Q</a>
+                            onClick={(event) => { setVisible(!isVisible);  setId(event.target.id)}}
+                            id='2'
+                        >Other</p>
                     </li>
                 </ul>
                 {isVisible ? 
-                    <div className='popupModal'>
+                    <motion.div className='popupModal'
+                        drag
+                        dragConstraints={{
+                            left: -200,
+                            right: 0,
+                            top: -200,
+                            bottom: 0
+                        }}
+                    >
                         <input
                             className='closeBtn'
                             type='button'
-                            onClick={() => setVisible(!isVisible)}
+                            onClick={() => { setVisible(!isVisible); console.log('poop')}}
                             value='X'
+                            id='buttonInput'
                         />
-                        <p>content here</p>
-                    </div>
+                        
+                        <h3>{itemsList[id].title}</h3>
+                        <p>{itemsList[id].content}</p>
+                        
+                    </motion.div>
                 : null}
             </div>
         </div>
     )
 }
+const itemsList = [
+    
+    {
+        title: 'Contact',
+        id: 0,
+        content: 'Office: 123-456-7890'
+    },
+    {
+        title: 'FAQ',
+        id: 1,
+        content:'Q: poop? A: yes'
+    },
+    {
+        title: 'Poop',
+        id: 2,
+        content: 'dick peen'
+    }
+    
+  ]
+  
